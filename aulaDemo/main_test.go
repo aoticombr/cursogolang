450c2e48445e74692faf39adb96cf0b795e38ba9
krobrelus <krobrelus@gmail.com>
2025-09-25 20:32:56 -0400

    teste

--- aulaDemo/main_test.go
+++ aulaDemo/main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-	resultado := Soma(2, 3)
+	resultado := Soma(3, 2)
 	esperado := 5
 	if resultado != esperado {
 		t.Errorf("Soma(2, 3) = %d; esperado %d", resultado, esperado)
